0229403ee4c3eb2629fba8b49d8c37b72d2d3809
sittidetP <sittidet@odds.team>
2023-12-02 16:24:38 +0700

    add mockPokemonList

--- app/src/main/java/com/example/pokemon/PokemonCollectionActivity.kt
+++ app/src/main/java/com/example/pokemon/PokemonCollectionActivity.kt
@@ -2,7 +2,10 @@ package com.example.pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokemon.adapter.PokemonAdapter
 import com.example.pokemon.databinding.ActivityPokemonCollectionBinding
+import com.example.pokemon.model.Pokemon
+import com.example.pokemon.model.mockPokemonList
 
 class PokemonCollectionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPokemonCollectionBinding
@@ -11,5 +14,8 @@ class PokemonCollectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPokemonCollectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val adapter = PokemonAdapter(mockPokemonList())
+        binding.pokemonListRecyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/pokemon/model/Pokemon.kt
+++ app/src/main/java/com/example/pokemon/model/Pokemon.kt
@@ -15,3 +15,55 @@ data class Name(
     val english: String,
     val japanese: String
 )
+
+fun mockPokemonList(): List<Pokemon> =
+    listOf(
+        Pokemon(
+            id = 1,
+            name = Name(
+                english = "Bulbasaur",
+                japanese = "フシギダネ"
+            ),
+            types = listOf(
+                "Grass",
+                "Poison"
+            ),
+            hp = 45,
+            attack = 49,
+            defense = 49,
+            speed = 45,
+            imageUrl = "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/001.png"
+        ),
+        Pokemon(
+            id = 2,
+            name = Name(
+                english = "Ivysaur",
+                japanese = "フシギソウ"
+            ),
+            types = listOf(
+                "Grass",
+                "Poison"
+            ),
+            hp = 60,
+            attack = 62,
+            defense = 63,
+            speed = 60,
+            imageUrl = "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/002.png"
+        ),
+        Pokemon(
+            id = 3,
+            name = Name(
+                english = "Venusaur",
+                japanese = "フシギバナ"
+            ),
+            types = listOf(
+                "Grass",
+                "Poison"
+            ),
+            hp = 80,
+            attack = 82,
+            defense = 83,
+            speed = 80,
+            imageUrl = "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/003.png"
+        ),
+    )
