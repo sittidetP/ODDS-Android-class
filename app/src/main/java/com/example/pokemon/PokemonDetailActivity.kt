0cbd72b823c41c9440d1e21417d51db28173925f
sittidetP <sittidet@odds.team>
2023-12-02 14:24:35 +0700

    create PokemonDetailActivity

---
+++ app/src/main/java/com/example/pokemon/PokemonDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokemonDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_detail)
+    }
+}
